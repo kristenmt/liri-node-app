17edd1a1833e1cf7dfa7929584284ea6108e12e6
Kristen Truyol <kristentruyol@Kristens-MacBook-Air.local>
2018-08-13 14:59:29 -0400

    functions are working

--- liri.js
+++ liri.js
@@ -8,20 +8,20 @@ var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 var request = require("request");
 var fs = require("fs");
-var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+
 
 //log to check keys are pulled correctly
 console.log("got keys loaded");
 
 //load artist's name in this variable when requested by user for spotify
-var getBandName = function(artist){
+var getBandName = function (artist) {
     return artist.name;
 };
 
 //load information from spotify in this variable when requested by user
-var getSpotify = function(songName){
+var getSpotify = function (songName) {
     //if there's an error
-    if (songName === undefined){
+    if (songName === undefined) {
         songName = "Not a song";
     }
     //define the spotify search items
@@ -31,13 +31,13 @@ var getSpotify = function(songName){
             query: songName
         },
         //function for spotify search
-        function(err, data){
-            if(err){
+        function (err, data) {
+            if (err) {
                 console.log("error :" + err);
                 return;
             }
             var songs = data.tracks.items;
-            for (var i=0; i < songs.length; i++){
+            for (var i = 0; i < songs.length; i++) {
                 console.log(i);
                 console.log("singer/band: " + songs[i].artists.map(getBandName));
                 console.log("song title: " + songs[i].name);
@@ -49,39 +49,65 @@ var getSpotify = function(songName){
     )
 }
 //load information from twitter in this variable when requested by user
-    var getTweets = function(){
-        //from this twitter handle
-        var params = {
-            screen_name: "MakeItMine3"
-        };
-    }
-    client.get("statuses/user_timeline", params, function(error, tweets, response){
-        if (!error){
-            for (var i = 0; i < tweets.length; i++){
+var getTweets = function () {
+    //from this twitter handle
+    var params = {
+        screen_name: "MakeItMine3"
+        //screen_name: twitterUsername
+    };
+
+    client.get("statuses/user_timeline", params, function (error, tweets, response) {
+        if (!error) {
+            for (var i = 0; i < tweets.length; i++) {
+                console.log("_____________________");
                 console.log(tweets[i].created_at);
-                
+                console.log(tweets[i].text);
+
             }
+
         }
     })
-//function to run movie search
-var getMovie = function(movieName){
-    if (movieName === undefined){
-        movieName = "Gone With the Wind";
-    }
-    request(movieName, function(error, response, body){
-        if (!error && response.statusCode === 200){
-            var jsonData = JSON.parse(body);
-            console.log("Title: " + jsonData.Title);
-            console.log("Year: " + jsonData.Year);
-            
-            
+};
+    //function to run movie search
+    var getMovie = function (y) {
+        var movieName = y;
+        var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+
+        if (movieName === undefined) {
+            movieName = "Mr. Nobody";
         }
-    }) 
+        request(queryUrl, function (error, response, body) {
+            if (!error && response.statusCode === 200) {
+                var jsonData = JSON.parse(body);
+                console.log("Title: " + jsonData.Title);
+                console.log("Year: " + jsonData.Year);
+            }
+        })
+    };
+    //the do what it says function
+    var doWhatItSays = function () {
+        fs.readFile("random.txt", "utf8", function (error, data) {
+            console.log(data);
+            var dataArr = data.split(", ");
+        })
+    };
+    //switch/case statement to determine which function will run
+    var chooseFunction = function (action, y) {
+        switch (action) {
+            case "my-tweets": getTweets();
+                break;
+            case "spotify-this-song": getSpotify(y);
+                break;
+            case "movie-this": getMovie(y);
+                break;
+            case "do-what-it-says": doWhatItSays();
+                break;
+        }
+    };
+
+function runLiri(x, y) {
+    console.log(x, y);
+
+    chooseFunction(x, y);
 }
-    // `my-tweets`
-
-    // `spotify-this-song`
-
-    // `movie-this`
-
-    // `do-what-it-says`
+runLiri(process.argv[2], process.argv[3]);
